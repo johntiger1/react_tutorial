4f8dffa707c09987da545511a58a415588c58178
johntiger1 <john.person1@gmail.com>
2018-07-11 21:13:07 -0400

    working state controlled in parent component, and propagageted along with function pointers down to subcomponents

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,24 +3,76 @@ import ReactDOM from 'react-dom';
 import './index.css';
 
 class Square extends React.Component {
+
+
+
   render() {
     return (
-      <button className="square">
+      <button className="square" 
+      onClick={()=>this.props.onClick() }>
         {/* TODO */}
 
-          { this.props.value + 69}
+          { this.props.value}
         {/* This is how you do some commenting in JSX
           <!--<p> Hello this is some text </p>-->
 */}
       </button>
     );
   }
+
+  // constructor(props){
+
+  //   super(props);
+
+  //   this.state = {
+
+  //     value: null,
+  //   };
+  // }
+  // function my_random_func(){
+  //     console.log("i am a regular old javascript func");
+
+  // }
 }
 
+
 class Board extends React.Component {
-  renderSquare(i) {
-    return <Square value={i} />;
+
+    constructor(props){
+      super(props);
+      this.state = {
+        squares: Array(9).fill(null),
+      };
+    }
+  handleClick(i)
+  {
+
+    console.log("call is being handled" + i);
+      const squares = this.state.squares.slice(); //create a copy only
+      squares[i] = 'X';
+
+      //dataflow: send the data back up (i.e. pass a copy up, rather than just modify it directly)
+      this.setState({squares: squares});
+
+      //this is aprt of a bigger mutable vs immutable discussion
+      //immutable allows easier state tracking and state-tracking
+      
+
+
   }
+  renderSquare(i) {
+    return (<Square 
+    value={this.state.squares[i]}
+    onClick={()=>this.handleClick(i)}
+     />);
+    }
+  
+
+
+
+// \how to bind functions:?
+  // random_func(){    console.log("random");
+  // }
 
   render() {
     const status = 'Next player: X';
