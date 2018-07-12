7e9339341e85e7bfc6a01b65b6b3dca69dbfd6b5
johntiger1 <john.person1@gmail.com>
2018-07-11 21:35:55 -0400

    working game; BL for determining whether we won or not

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -22,17 +22,31 @@ class Board extends React.Component {
       super(props);
       this.state = {
         squares: Array(9).fill(null),
+        xIsNext: true,
       };
     }
   handleClick(i)
   {
 
     console.log("call is being handled" + i);
+
+
+
       const squares = this.state.squares.slice(); //create a copy only
-      squares[i] = 'X';
+
+      if (calculateWinner(squares) || squares[i])
+      {
+        return;
+      }
+
+      squares[i] = this.state.xIsNext ? 'X' : 'O';
 
       //dataflow: send the data back up (i.e. pass a copy up, rather than just modify it directly)
-      this.setState({squares: squares});
+      this.setState(
+        {squares: squares,
+          xIsNext: !this.state.xIsNext,}
+
+        );
 
       //this is aprt of a bigger mutable vs immutable discussion
       //immutable allows easier state tracking and state-tracking
@@ -55,7 +69,10 @@ class Board extends React.Component {
   // }
 
   render() {
-    const status = 'Next player: X';
+    const winner = calculateWinner(this.state.squares);
+
+    let status = winner ? 'Winner is ' + winner :  'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    // const status = 'Next player: '+ (this.state.xIsNext ? 'X' : 'O');
 
     return (
       <div>
@@ -102,3 +119,24 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    //destructyring assignment, unpack the 3 elements in the array into the three elements
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
